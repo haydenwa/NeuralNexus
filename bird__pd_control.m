b03bb14b1847a1e441ed7077f820f0985fbd38b9
Joyce Lai <joycelai@umich.edu>
2024-04-03 15:13:43 -0400

    Update bird__pd_control.m

--- bird__pd_control.m
+++ bird__pd_control.m
@@ -3,8 +3,8 @@ clear, clc, close all
 rng(0);
 
 %% Define Plant (Continuous-time)
-I = 2119*1e-6; % [g*m^2]
-tp = 67.7*1e-3; % [sec]
+I = 2119; % [g*mm^2]
+tp = 69.7; % [ms]
 A = [0, 1; 0, 1/(I*tp)];
 B = [0; 1/I];
 [nx, nu] = size(B);
@@ -13,18 +13,19 @@ D = zeros(1, nu);
 
 G = ss(A,B,C,D)
 
-%% Define PD Controller
-kp = 1;
-kd = 40;
+%% Define PD/LQR Controller
+tc = 40; % PD controller time constant [ms]
+k = 1;
+kp = k;
+kd = k*tc;
 K = -[kp, kd];
 
 Q = 1e-4*eye(nx);
 R = 1e-3*eye(nu);
-% K = -lqr(A,B,Q,R);
+K = -lqr(A,B,Q,R);
 
 %% Time Delay (Padé Approximation)
-Tdel = 2e-6;
-% Tdel = 0;
+Tdel = 10;
 ord = 2;
 s = tf('s');
 delay = exp(-Tdel*s);
@@ -51,8 +52,8 @@ Daug = [zeros(ny, ny+nu+ny);
 Tcl = ss(Aaug, Baug, Caug, Daug);
 
 %% Simulate PD Controller
-Ts = 1e-4;
-tspan = 0:Ts:500;
+Ts = 1e-3; % ms
+tspan = 0:Ts:1000; % ms
 Nt = length(tspan);
 
 % Define reference command
@@ -65,10 +66,14 @@ end
 
 % Generate disturbance and noise
 d = zeros(Nt, nu);
-sigma_n = 1e-8*eye(ny);
-RR = chol(sigma_n);
-n = randn(Nt, ny)*RR;
-% n = zeros(Nt, ny);
+cov_param = 1e-6;
+if cov_param == 0
+    n = zeros(Nt, ny);
+else
+    sigma_n = cov_param*eye(ny);
+    RR = chol(sigma_n);
+    n = randn(Nt, ny)*RR;
+end
 
 % Simulate 
 Y = lsim(Tcl, [r, d, n], tspan);
@@ -85,40 +90,40 @@ f.Position = [300 250 800 600];
 subplot(2,2,1)
 plot(tspan, x(1,:), linewidth=1.5);
 grid on
-xlabel('Time [sec]');
+xlabel('Time [msec]');
 ylabel('Angle [rad]');
-title('Simulated Pitch Angle');
+title('Pitch Angle');
 legend('$$\theta(t)$$', interpreter='latex');
 
 subplot(2,2,2)
 plot(tspan, x(2,:), linewidth=1.5);
 grid on
-xlabel('Time [sec]');
+xlabel('Time [msec]');
 ylabel('Angular Velocity [rad/sec]');
-title('Simulated Pitch Angular Velocity');
+title('Pitch Angular Velocity');
 legend('$$\dot{\theta}(t)$$', interpreter='latex');
 
 subplot(2,2,3)
 plot(tspan, xm(1,:), linewidth=1.5);
 grid on
-xlabel('Time [sec]');
+xlabel('Time [msec]');
 ylabel('Angle [rad]');
-title('Measured Pitch Angle');
+title('Delayed/Noisy Pitch Angle');
 legend('$${\theta}_m(t)$$', interpreter='latex');
 
 subplot(2,2,4)
 plot(tspan, xm(2,:), linewidth=1.5);
 grid on
-xlabel('Time [sec]');
+xlabel('Time [msec]');
 ylabel('Angular Velocity [rad/sec]');
-title('Measured Pitch Angular Velocity');
+title('Delayed/Noisy Pitch Angular Velocity');
 legend('$${\dot{\theta}}_m(t)$$', interpreter='latex');
 
 
 figure(2)
 plot(tspan, u, linewidth=1.5);
 grid on
-xlabel('Time [sec]');
+xlabel('Time [msec]');
 ylabel('Torque [Nm]');
 legend('$$u(t)$$', interpreter='latex')
 title('Control Input');
@@ -126,14 +131,14 @@ title('Control Input');
 figure(3)
 plot(tspan, c, linewidth=1.5);
 grid on
-xlabel('Time [sec]');
+xlabel('Time [msec]');
 legend('$$c(t)$$', interpreter='latex');
 title('Per-step Cost');
 
 figure(4)
 plot(tspan, J, linewidth=1.5);
 grid on
-xlabel('Time [sec]');
+xlabel('Time [msec]');
 legend('$$J(t)$$', interpreter='latex');
 title('Total Cost');
 
